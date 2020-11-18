ecb116ecc2311818e78d4d8ced6fc74fdbf29436
Spiffy02 <74658123+Spiffy02@users.noreply.github.com>
2020-11-18 14:18:34 +0100

    Add files via upload
    Opgeschoonde code, engels vertaald, gamelist tkinter werkt

--- Sprint1.py
+++ Sprint1.py
@@ -1,55 +1,52 @@
-import json
-from tkinter import *
 import requests
 from tkinter import ttk
 import tkinter as tk
 
-def json_file_openen():
+
+def open_jsonfile():
     url = "https://raw.githubusercontent.com/tijmenjoppe/AnalyticalSkills-student/master/project/data/steam.json"
     r = requests.get(url)
-    filedata = r.json()
-    return filedata
+    fileData = r.json()
+    return fileData
 
 
 def tkinter():
-    game = first_game()
+    gameList = sorted_data()
     root = tk.Tk()
     root.resizable(width=1, height=1)
-    treev = ttk.Treeview(root, selectmode='browse')
-    treev.pack(side='top')
-    scrollbar = ttk.Scrollbar(root,
-                              orient="vertical",
-                              command=treev.yview)
+    treeView = ttk.Treeview(root, selectmode='browse')
+    scrollbar = ttk.Scrollbar(root, orient="vertical", command=treeView.yview)
+    treeView.pack(side='top')
     scrollbar.pack(side='right', fill='x')
-    treev.configure(xscrollcommand=scrollbar.set)
-    treev["columns"] = ("1")
-    treev['show'] = 'headings'
-    treev.column("1", width=90, anchor='w')
-    treev.heading("1", text="Game Naam")
-    treev.insert("", 'end', text='Game',
-                 values=game)
+    treeView.configure(xscrollcommand=scrollbar.set)
+    treeView["columns"] = "1"
+    treeView['show'] = 'headings'
+    treeView.column("1", width=250)
+    treeView.heading("1", text="Game Naam:")
+    for games in gameList:
+        if "\"" not in games:
+            games = "\"" + games + "\""
+        treeView.insert("", 'end', text="Game", values=games)
     root.geometry("750x750")
     root.mainloop()
 
+
 def first_game():
-    filedata = json_file_openen()
-    data = filedata[0]
+    fileData = open_jsonfile()
+    data = fileData[0]
     name = data["name"]
     return name
 
-def gesorteerde_data():
-    list = []
-    filedate = json_file_openen()
-    num = 0
-    for data in filedate:
-        data = filedate[num]
-        namen = data["name"]
-        num += 1
-        list.append(namen)
-    gesorteerde = sorted(list)
 
-    return gesorteerde
-tkinter()
+def sorted_data():
+    gameList = []
+    fileData = open_jsonfile()
+    for data in fileData:
+        name = data["name"]
+        gameList.append(name)
+    return sorted(gameList)
+
+
 print(first_game())
-json_file_openen()
-print(gesorteerde_data())
+open_jsonfile()
+tkinter()
